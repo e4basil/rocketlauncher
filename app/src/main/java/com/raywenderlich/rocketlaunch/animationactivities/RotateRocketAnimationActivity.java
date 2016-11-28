eb3dc358b8a03c71d977bd68cd856f4ed17676d1
Basil K <e4basil@gmail.com>
2016-11-28 17:43:18 +0530

    RotateRocketAnimation completed

--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/RotateRocketAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/RotateRocketAnimationActivity.java
@@ -1,8 +1,23 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
-public class RotateRocketAnimationActivity extends BaseAnimationActivity {
-  @Override
-  protected void onStartAnimation() {
+import android.animation.ValueAnimator;
+import android.view.animation.LinearInterpolator;
 
-  }
+public class RotateRocketAnimationActivity extends BaseAnimationActivity {
+    @Override
+    protected void onStartAnimation() {
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, -360);
+
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                mRocket.setRotation(value);
+            }
+        });
+
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.setDuration(DEFAULT_ANIMATION_DURATION);
+        valueAnimator.start();
+    }
 }
