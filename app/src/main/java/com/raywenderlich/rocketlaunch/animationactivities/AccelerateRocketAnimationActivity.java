5f36a0a4732e3f50a49ffa8b62e81dbf9dfbb69c
Basil K <e4basil@gmail.com>
2016-11-28 19:58:51 +0530

    AccelerateRocketAnimation completed

--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/AccelerateRocketAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/AccelerateRocketAnimationActivity.java
@@ -1,8 +1,22 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
-public class AccelerateRocketAnimationActivity extends BaseAnimationActivity {
-  @Override
-  protected void onStartAnimation() {
+import android.animation.ValueAnimator;
+import android.view.animation.AccelerateInterpolator;
 
-  }
+public class AccelerateRocketAnimationActivity extends BaseAnimationActivity {
+    @Override
+    protected void onStartAnimation() {
+        ValueAnimator animator = ValueAnimator.ofFloat(0, -mScreenHeight);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                mRocket.setTranslationY(value);
+            }
+        });
+
+        animator.setInterpolator(new AccelerateInterpolator(1.5f));
+        animator.setDuration(DEFAULT_ANIMATION_DURATION);
+        animator.start();
+    }
 }
